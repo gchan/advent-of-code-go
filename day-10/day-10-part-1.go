f33e3df76a21c287319f0ababd062603fac5cbc0
Gordon Chan <developer.gordon+github@gmail.com>
2016-01-02 15:05:25 +1300

    Day 10 and 11

---
+++ day-10/day-10-part-1.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+  "io/ioutil"
+  "strconv"
+)
+
+func main() {
+  numbers, err := ioutil.ReadFile("./day-10-input.txt")
+  if err != nil { panic(err) }
+
+  for i := 0; i < 40; i++ {
+    new_numbers := make([]byte, 0)
+
+    for j := 0; j < len(numbers); j++ {
+      num := numbers[j]
+
+      start := j
+      for j < len(numbers) - 1 && numbers[j + 1] == num {
+        j++
+      }
+      end := j
+
+      length := end - start + 1
+
+      new_numbers = append(new_numbers, strconv.Itoa(length)[0])
+      new_numbers = append(new_numbers, numbers[j])
+    }
+
+    numbers = new_numbers
+  }
+
+  println(len(numbers))
+}
