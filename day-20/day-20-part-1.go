80207a6056fcb4e286c954df145987e700c1fc36
Gordon Chan <developer.gordon+github@gmail.com>
2016-01-09 21:57:07 +1300

    Day 20

---
+++ day-20/day-20-part-1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "io/ioutil"
+  "strconv"
+)
+
+func main() {
+  input, err := ioutil.ReadFile("./day-20-input.txt")
+  if err != nil { panic(err) }
+
+  target, _ := strconv.Atoi(string(input))
+  houses := make([]int, target)
+
+  for elf := 1; elf <= target / 10; elf++ {
+    upto := target / 10 / elf
+    for number := 1; number <= upto; number++ {
+      houses[number * elf] += elf * 10
+    }
+  }
+
+  for i := 1; i < len(houses); i++ {
+    presents := houses[i]
+    if presents >= target {
+      println(i)
+      break
+    }
+  }
+}
