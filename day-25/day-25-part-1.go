3bc49d21305435e6465f51f461ef903e028f5149
Gordon Chan <developer.gordon+github@gmail.com>
2016-01-14 20:47:03 +1300

    Day 25

---
+++ day-25/day-25-part-1.go
@@ -0,0 +1,30 @@
+package main
+
+func main() {
+  row := 1
+  col := 1
+
+  count := 0
+
+  for !(row == 2978 && col == 3083) {
+    col += 1
+    row -= 1
+
+    if row == 0 {
+      row = col
+      col = 1
+    }
+
+    count++;
+  }
+
+  prev_code := 20151125
+  code := 0
+
+  for i := 0; i < count; i++ {
+    code = (prev_code * 252533) % 33554393
+    prev_code = code
+  }
+
+  println(code)
+}
