d5b7608bd02be20d2c986c276dbfa5d2ff48d8d7
Gordon Chan <developer.gordon+github@gmail.com>
2016-12-06 18:52:51 +1300

    Day 6 2016

---
+++ 2016/day-06/day-06-part-1.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"io/ioutil"
+	"strings"
+)
+
+func main() {
+	input, _ := ioutil.ReadFile("./day-06-input.txt")
+
+	lines := strings.Split(string(input), "\n")
+
+	var charFreq [8]map[rune]int
+	var message string
+
+	for idx := range charFreq {
+		charFreq[idx] = make(map[rune]int)
+	}
+
+	for _, line := range lines {
+		for idx, char := range line {
+			charFreq[idx][char]++
+		}
+	}
+
+	for _, charCount := range charFreq {
+		var bestCount int
+		var bestChar rune
+
+		for char, count := range charCount {
+			if count > bestCount {
+				bestCount = count
+				bestChar = char
+			}
+		}
+
+		message += string(bestChar)
+	}
+
+	println(message)
+}
